44b2da24be833918de7672527481c7818c58ec36
Elan Hamburger <elan.hamburger@gmail.com>
2019-11-18 22:56:49 -0800

    Build

---
+++ build/precache-manifest.2932e426e1cf39e7aa9a7b8a6e640170.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab411e5e3b5297c8ff999971dc8cb085",
+    "url": "/static/media/walk.ab411e5e.svg"
+  },
+  {
+    "revision": "63c6e86f348f488c5c98",
+    "url": "/static/css/main.d4adf1ce.chunk.css"
+  },
+  {
+    "revision": "7a9d9ed3272f34acd61a",
+    "url": "/static/js/1.7a9d9ed3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45d60d07056f0800759baead19acfdb1",
+    "url": "/static/media/bus.45d60d07.svg"
+  },
+  {
+    "revision": "aa7f53c06bf65df24ce8e838f8d74668",
+    "url": "/static/media/my_location.aa7f53c0.svg"
+  },
+  {
+    "revision": "63c6e86f348f488c5c98",
+    "url": "/static/js/main.63c6e86f.chunk.js"
+  },
+  {
+    "revision": "db23af8c2171fe9a0006061b9cf94684",
+    "url": "/static/media/close.db23af8c.svg"
+  },
+  {
+    "revision": "e27192cbdd7dbc4012199afa6ba27175",
+    "url": "/static/media/marker.e27192cb.svg"
+  },
+  {
+    "revision": "99fd2984c298053b07b1a0ed09a2c1a1",
+    "url": "/static/media/directions.99fd2984.svg"
+  },
+  {
+    "revision": "22547ff556584ec6523bd2950b5c049a",
+    "url": "/static/media/close_white.22547ff5.svg"
+  },
+  {
+    "revision": "478fb7813781d2bcd19ce4576aa33334",
+    "url": "/static/media/swap.478fb781.svg"
+  },
+  {
+    "revision": "db1002087b3da8154f74b2f5cab191e4",
+    "url": "/index.html"
+  }
+];
