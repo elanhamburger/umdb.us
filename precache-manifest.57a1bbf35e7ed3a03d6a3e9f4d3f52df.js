76b012e6424c25100c36ccb13285f7c921e14db8
Elan Hamburger <elan.hamburger@gmail.com>
2019-11-19 08:06:35 +0000

    Deploy of commit cb8595b

---
+++ precache-manifest.57a1bbf35e7ed3a03d6a3e9f4d3f52df.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab411e5e3b5297c8ff999971dc8cb085",
+    "url": "/static/media/walk.ab411e5e.svg"
+  },
+  {
+    "revision": "018495aafd52eb3bb7a6",
+    "url": "/static/css/main.b54e209b.chunk.css"
+  },
+  {
+    "revision": "7a9d9ed3272f34acd61a",
+    "url": "/static/js/1.7a9d9ed3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45d60d07056f0800759baead19acfdb1",
+    "url": "/static/media/bus.45d60d07.svg"
+  },
+  {
+    "revision": "aa7f53c06bf65df24ce8e838f8d74668",
+    "url": "/static/media/my_location.aa7f53c0.svg"
+  },
+  {
+    "revision": "018495aafd52eb3bb7a6",
+    "url": "/static/js/main.018495aa.chunk.js"
+  },
+  {
+    "revision": "db23af8c2171fe9a0006061b9cf94684",
+    "url": "/static/media/close.db23af8c.svg"
+  },
+  {
+    "revision": "e27192cbdd7dbc4012199afa6ba27175",
+    "url": "/static/media/marker.e27192cb.svg"
+  },
+  {
+    "revision": "99fd2984c298053b07b1a0ed09a2c1a1",
+    "url": "/static/media/directions.99fd2984.svg"
+  },
+  {
+    "revision": "22547ff556584ec6523bd2950b5c049a",
+    "url": "/static/media/close_white.22547ff5.svg"
+  },
+  {
+    "revision": "478fb7813781d2bcd19ce4576aa33334",
+    "url": "/static/media/swap.478fb781.svg"
+  },
+  {
+    "revision": "43495082f8c3bdb689e25cc409a540ab",
+    "url": "/index.html"
+  }
+];
